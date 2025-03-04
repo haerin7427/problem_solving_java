1a9436374f3b6e24fc4aa97e04ce8a4027dbb430
haerin7427 <hae7427@gmail.com>
2025-03-04 23:59:44 +0900

    Feat: 구간 합 구하기4 문제풀이

---
+++ PrefixSum/baekjoon/No_11659.java
@@ -0,0 +1,31 @@
+package PrefixSum.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_11695 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        int[] sum = new int[N+1];
+        for(int i=1; i<=N; i++) {
+            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            System.out.println(sum[end] - sum[start-1]);
+        }
+        
+    }
+    
+}
