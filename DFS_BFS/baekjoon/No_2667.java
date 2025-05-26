7f7b216f8262e9cc4be874532ca26e7576880d6b
haerin7427 <hae7427@gmail.com>
2025-05-26 23:12:02 +0900

    Feat: [DFS/BFS] "단지번호붙이기" 문제 풀이

--- DFS_BFS/baekjoon/No_2667.java
+++ DFS_BFS/baekjoon/No_2667.java
@@ -1,7 +1,7 @@
 package DFS_BFS.baekjoon;
 
 // 문제 : 단지번호붙이기
-// 풀이 일자 : 2025.03.19
+// 풀이 일자 : 2025.03.19, 2025.05.26
 // 설명 : https://www.acmicpc.net/problem/2667
 
 import java.io.*;
@@ -12,58 +12,81 @@ public class No_2667 {
     private static int[][] move = new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
     private static int[][] board;
     private static boolean[][] visited;
-    private static int count_house;
-    private static int N;
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        
+        int n = Integer.parseInt(br.readLine());
 
-        board = new int[N][N];
-        for(int i=0; i<N; i++) {
-            String str = br.readLine();
-            for(int j=0; j<N; j++) {
-                board[i][j] = str.charAt(j) == '0' ? 0 : 1;
+        board = new int[n][n];
+        for(int y=0; y<n; y++) {
+            String info = br.readLine();
+            for(int x=0; x<n; x++) {
+                board[y][x] = info.charAt(x) - '0';
             }
         }
 
+        visited = new boolean[n][n];
+        List<Integer> answer = bfs();
+        print_answer(answer);
+    }
 
-        int count_section = 0;
-        visited = new boolean[N][N];
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                if(board[i][j] == 1 && !visited[i][j]) {
-                    count_section += 1;
-                    count_house = 0;
-                    dfs(i, j);
-                    list.add(count_house);
+    private static class Node {
+        int y;
+        int x;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private static List<Integer> bfs() {
+
+        List<Integer> houce_list = new ArrayList<>();
+        
+        for(int y=0; y<board.length; y++) {
+            for(int x=0; x<board[0].length; x++) {
+
+                if(board[y][x] == 0 || visited[y][x]) continue;
+                
+                Queue<Node> queue = new LinkedList<>();
+                queue.add(new Node(x, y));
+                visited[y][x] = true;
+                int count = 1;
+                while (!queue.isEmpty()) {
+                    Node current_node = queue.remove();
+
+                    for(int[] d : move) {
+                        int new_y = current_node.y + d[0];
+                        int new_x = current_node.x + d[1];
+
+                        if(new_y < 0 || new_y >= board.length || new_x < 0 || new_x >= board[0].length) continue;
+                        if(board[new_y][new_x] == 0 || visited[new_y][new_x]) continue;
+
+                        count += 1;
+                        visited[new_y][new_x] = true;
+                        queue.add(new Node(new_x, new_y));
+                    }
                 }
+                houce_list.add(count);
             }
         }
 
-        StringBuilder answer = new StringBuilder();
-        answer.append(count_section).append("\n");
-        Collections.sort(list);
-        for(int num : list){
-            answer.append(num).append("\n");
-        }
-        System.out.println(answer);
+        return houce_list;
     }
 
-    public static void dfs (int y, int x) {
-        visited[y][x] = true;
-        count_house += 1;
-
-        for(int i=0; i<move.length; i++){
-            int newY = y + move[i][0];
-            int newX = x + move[i][1];
-
-            if(newX < 0 || newX >= N || newY < 0 || newY >= N) continue;
-            if(board[newY][newX] == 0 || visited[newY][newX]) continue;
-
-            dfs(newY, newX);
+    private static void print_answer(List<Integer> values) {
+        StringBuffer sb = new StringBuffer();
+        Collections.sort(values);
+        sb.append(values.size() +"\n");
+        for(int a : values) {
+            sb.append(a +"\n");
         }
+        System.out.print(sb.toString());
+
     }
+
+    
 }
