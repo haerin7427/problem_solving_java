265b17e82cbbdcc7c0af3ced0e07aed7741ed1e6
haerin7427 <hae7427@gmail.com>
2025-06-08 21:49:21 +0900

    Feat: [Implementation] '지구 온난화' 다시풀이

--- Implementation/baekjoon/No_5212.java
+++ Implementation/baekjoon/No_5212.java
@@ -1,80 +1,78 @@
 package Implementation.baekjoon;
 
 // 문제 : 지구 온난화
-// 풀이 일자 : 2025.05.20
+// 풀이 일자 : 2025.06.08(2)
 // 설명 : https://www.acmicpc.net/problem/5212
 
 import java.util.*;
 import java.io.*;
 
 public class No_5212 {
-    static List<String> current_map = new LinkedList<>();
-    static char[][] after50_map;
-    static int r, c;
-    static int min_x=9, max_x=0, min_y=9, max_y=0;
+    static char[][] initial;
+    static char[][] after;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<r; i++) {
-            current_map.add(br.readLine());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        initial = new char[r][c];
+        after = new char[r][c];
+
+        for(int y=0; y<r; y++){
+            String str = br.readLine();
+            for(int x=0; x<c; x++){
+                initial[y][x] = str.charAt(x);
+            }
         }
 
-        after50_map = new char[r][c];
-        for(int i=0; i<r; i++) {
-            for(int j=0; j<c; j++) {
-                if(current_map.get(i).charAt(j) == 'X') {
-                    check_land(i, j);
-                } else {
-                    after50_map[i][j] = '.';
+        int minX=9, maxX=0, maxY=0, minY=9;
+        for(int y=0; y<initial.length; y++){
+            for(int x=0; x<initial[0].length; x++){
+                after[y][x] = initial[y][x] == '.' ? '.' : check_land(y, x);
+
+                if(after[y][x] == 'X') {
+                    if(minX > x) {
+                        minX = x;
+                    }
+                    if(maxX < x) {
+                        maxX = x;
+                    }
+                    if(minY > y) {
+                        minY = y;
+                    }
+                    if(maxY < y) {
+                        maxY = y;
+                    }
                 }
             }
         }
+
         StringBuffer answer = new StringBuffer();
-        for(int y = min_y; y<=max_y; y++) {
-            for(int x = min_x; x<=max_x; x++) {
-                answer.append(after50_map[y][x]);
+        for(int y=minY; y<=maxY; y++) {
+            for(int x=minX; x<=maxX; x++) {
+                answer.append(after[y][x]);
             }
-            answer.append("\n");
+            answer.append('\n');
         }
         System.out.print(answer.toString());
-    }    
-    private static void check_land(int y, int x) {
+    }
 
-        int count_sea = 0;
+    static char check_land(int y, int x) {
+        int count=0;
+        int[][] move = new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
+        for(int[] d : move) {
+            int newY = y + d[0];
+            int newX = x + d[1];
 
-        if(y-1 < 0 || current_map.get(y-1).charAt(x) == '.') {
-            count_sea += 1;
-        }
-        if(x-1 < 0 || current_map.get(y).charAt(x-1) == '.') {
-            count_sea += 1;
-        }
-        if(y+1 >= r || current_map.get(y+1).charAt(x) == '.') {
-            count_sea += 1;
-        }
-        if(x+1 >= c || current_map.get(y).charAt(x+1) == '.') {
-            count_sea += 1;
-        }
-
-        if(count_sea >= 3) {
-            after50_map[y][x] = '.';
-        }else {
-            after50_map[y][x] = 'X';
-            if(min_x > x) {
-                min_x = x;
-            }
-            if(max_x < x) {
-                max_x = x;
-            }
-            if(min_y > y) {
-                min_y = y;
-            }
-            if(max_y < y) {
-                max_y = y;
+            if(newY < 0 || newY >= initial.length || newX < 0 || newX >= initial[0].length) {
+                count += 1;
+            }else if(initial[newY][newX] == '.') {
+                count += 1;
             }
         }
+        return count >= 3 ? '.' : 'X';
     }
 }
