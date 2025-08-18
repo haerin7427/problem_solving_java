a273ac8d42f5f0ac280acf74e4ee4b6c644dc21d
haerin7427 <hae7427@gmail.com>
2025-08-18 23:17:10 +0900

    Feat: [SQL] '모든 레코드 조회하기' 문제풀이

---
+++ SQL/Programmers/No_59034.sql
@@ -0,0 +1,8 @@
+-- 문제 : 모든 레코드 조회하기
+-- 풀이 일자 : 2025.08.18
+-- 설명 : https://school.programmers.co.kr/learn/courses/30/lessons/59034
+
+SELECT ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+;
