e086bd87bb281e87b0a907262007434518ef68ea
haerin7427 <hae7427@gmail.com>
2025-10-28 20:37:53 +0900

    Feat: [SQL] '조건별로 분류하여 주문상태 출력하기' 문제풀이

---
+++ SQL/Programmers/No_131113.sql
@@ -0,0 +1,15 @@
+-- 문제 : 조건별로 분류하여 주문상태 출력하기
+-- 풀이 일자 : 2025.10.28
+-- 설명 : https://school.programmers.co.kr/learn/courses/30/lessons/131113
+
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, "%Y-%m-%d") OUT_DATE, 
+    CASE 
+        WHEN OUT_DATE IS NULL
+            THEN '출고미정'
+        WHEN OUT_DATE <= '2022-05-01'
+            THEN '출고완료'
+        ELSE '출고대기'
+    END AS 출고여부
+FROM FOOD_ORDER
+ORDER BY ORDER_ID ASC
+;
