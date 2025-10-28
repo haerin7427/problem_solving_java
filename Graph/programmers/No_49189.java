eba9de4d116dcc058671600f29a118ee89e96fb0
haerin7427 <hae7427@gmail.com>
2025-10-28 21:12:29 +0900

    Feat: [Implementation] '가장 먼 노드' 문제풀이

--- Graph/programmers/No_49189.java
+++ Graph/programmers/No_49189.java
@@ -1,60 +1,53 @@
 package Graph.programmers;
 
 // 문제 : 가장 먼 노드
-// 풀이 일자 : 2025.08.09
+// 풀이 일자 : 2025.08.09, 2025.10.28
 // 설명 : https://school.programmers.co.kr/learn/courses/30/lessons/49189
 
 import java.util.*;
 
 public class No_49189 {
     class Solution {
-        private class Node {
+        class Node {
             int value;
             int distance;
             
-            Node(int v, int d) {
-                value = v;
-                distance = d;
+            Node (int v, int d) {
+                this.value = v;
+                this.distance = d;
             }
         }
-        
         public int solution(int n, int[][] edge) {
-            List<Integer>[] list = new ArrayList[n+1];
-            for(int i=0; i<=n; i++) {
-                list[i] = new ArrayList<>();
-            }
-            
-            for(int[] o : edge) {
-                list[o[0]].add(o[1]);
-                list[o[1]].add(o[0]);
-            }
-            
-            return bfs(list, n);
-        }
-        
-        private int bfs(List<Integer>[] list, int n) {
-            Queue<Node> queue = new LinkedList<>();
             boolean[] visited = new boolean[n+1];
-            visited[1] = true;
-            queue.add(new Node(1, 0));
+            List<Integer>[] list = new LinkedList[n+1];
+            for(int i=0; i<=n; i++) {
+                list[i] = new LinkedList<>();
+            }
             
-            int answer = 0;
+            for(int[] e : edge) {
+                list[e[0]].add(e[1]);
+                list[e[1]].add(e[0]);
+            }
+            
+            Queue<Node> q = new LinkedList<>();
+            q.add(new Node(1, 0));
+            visited[1] = true;
+            
             int max_distance = 0;
-            while(!queue.isEmpty()) {
-                Node current_node = queue.remove();
-                
-                if(max_distance < current_node.distance) {
-                    max_distance = current_node.distance;
+            int answer = 1;
+            while(!q.isEmpty()){
+                Node cur = q.remove();
+                if(max_distance < cur.distance) {
+                    max_distance = cur.distance;
                     answer = 1;
-                }else if(max_distance == current_node.distance) {
+                }else if(max_distance == cur.distance) {
                     answer += 1;
                 }
                 
-                for(int next : list[current_node.value]){
-                    if(visited[next]) continue;
-                    
-                    visited[next] = true;
-                    queue.add(new Node(next, current_node.distance+1));
+                for(int value : list[cur.value]) {
+                    if(visited[value]) continue;
+                    visited[value] = true;
+                    q.add(new Node(value, cur.distance+1));
                 }
             }
             return answer;
