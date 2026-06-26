3517b3b726c71ee0ea49408e031a428e7dc9f73e
haerin7427 <hae7427@gmail.com>
2026-06-26 11:33:38 +0900

    Docs: [Sort] '가장 큰 수' 복습

--- Sort/programmers/No_42746.java
+++ Sort/programmers/No_42746.java
@@ -1,7 +1,7 @@
 package Sort.programmers;
 
 // 문제 : 가장 큰 수
-// 풀이 일자 : 2025.08.09
+// 풀이 일자 : 2025.08.09, 2026.06.25
 // 설명 : https://school.programmers.co.kr/learn/courses/30/lessons/42746
 
 import java.util.*;
@@ -33,6 +33,33 @@ public class No_42746 {
             }else {
                 return sb.toString();
             }
+        }
+    }
+
+    // 2026.06.25
+    class Solution2 {
+        public String solution(int[] numbers) {
+            int n = numbers.length;
+            String[] numbers_string = new String[n];
+            for(int i=0; i<n; i++) {
+                numbers_string[i] = String.valueOf(numbers[i]);
+            }
+            
+            Arrays.sort(numbers_string, new Comparator<String>(){
+                @Override
+                public int compare(String o1, String o2) {
+                    return Integer.valueOf(o2 + o1) - Integer.valueOf(o1 + o2);
+                }
+            });
+
+            
+            StringBuffer answer = new StringBuffer();
+            if(numbers_string[0].equals("0")) return "0";
+            for(int i=0; i<n; i++) {
+                answer.append(numbers_string[i]);
+                
+            }
+            return answer.toString();
         }
     }
 }
